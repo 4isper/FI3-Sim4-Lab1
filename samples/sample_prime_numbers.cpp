32f6e5959f0c0e8a0f82534af5cad95e7752759e
4isper <90023626+4isper@users.noreply.github.com>
2023-02-21 07:33:35 +0000

    OGo

---
+++ samples/sample_prime_numbers.cpp
@@ -0,0 +1,21 @@
+﻿// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+//
+// sample_prime_numbers.cpp - Copyright (c) Гергель В.П. 20.08.2000
+//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
+//
+// Тестирование битового поля и множества
+
+#include <iomanip>
+
+// #define USE_SET // Использовать класс TSet,
+                // закоментировать, чтобы использовать битовое поле
+
+#ifndef USE_SET // Использовать класс TBitField
+
+#include "polynom.h"
+
+int main()
+{
+  return 0;
+}
+#endif
---
+++ test/test_main.cpp
@@ -0,0 +1,11 @@
+#include <gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+//test_polynom 
+//polynom.cpp 
+//polynom.h 
+/*TEST(TSet, can_get_max_power_set);{ 
+ EXPECT_EQ(3, (1,2));*/
