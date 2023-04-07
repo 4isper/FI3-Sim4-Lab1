9eb690c0e5a507bb9371cbcde4155878ae0ea4c7
Kirill23323 <KirillProkofev12345@yandex.ru>
2023-04-07 14:18:29 +0300

    FinalHashTable

--- include/HashTable.h
+++ include/HashTable.h
@@ -1,8 +1,19 @@
+/*Status of the table row
+1 - line is busy
+2 - line is free
+3 - line deleted
+4 - row outside the table(for the insertion algorithm)
+*/
+
 #include "IMap.h"
 #include "Item.h"
 
 #ifndef __HashTable_H__
 #define __HashTable_H__
+
+template <class Key, class Data>										
+class THashTableIterator;
+
 template <class Key, class Data>
 class THashTable : public IMap<Key,Data>
 {
@@ -10,10 +21,10 @@ public:
 	THashTable(int size_table = 10);
 	THashTable(const THashTable<Key, Data>& m);
 	~THashTable();
-	int HashFunction(Key* k, int table_size);
-	int HashFunction(Key* k, int table_size) const;
-	int DoubleHashFunction(int table_size,int prev_index);
-	int DoubleHashFunction(int table_size,int prev_index) const;
+	int HashFunction(Key* k);
+	int HashFunction(Key* k) const;
+	int DoubleHashFunction(int prev_index);
+	int DoubleHashFunction(int prev_index) const;
 	void Add(Key* k, Data* d);
 	Data* Find(Key* k);
 	const Data* Find(Key* k) const;
@@ -22,8 +33,11 @@ public:
 	const Data* operator[](Key* k) const;
 	int FindInd(Key* k);
 	int FindInd(Key* k) const;
+	THashTableIterator<Key, Data> Begin();
+	THashTableIterator<Key, Data> End();
 protected:
 	TItem<Key, Data>* items;
+	friend class THashTableIterator<Key, Data>;
 };
 
 template<class Key, class Data>
@@ -33,9 +47,10 @@ inline THashTable<Key, Data>::THashTable(int size_table)
 	{
 		this->size = size_table;
 		this->count = 0;
-		items = new TItem<Key, Data>[this->size];
+		items = new TItem<Key, Data>[this->size + 1];
 		for (int i = 0; i < this->size; i++)
 			items[i].SetState(2);
+		items[size].SetState(4);
 	}
 	else
 	{
@@ -66,35 +81,31 @@ inline THashTable<Key, Data>::~THashTable()
 }
 
 template<class Key, class Data>
-inline int THashTable<Key, Data>::HashFunction(Key* k, int table_size)
+inline int THashTable<Key, Data>::HashFunction(Key* k)
 {
-	int sum = 0;
-	for (char ch : *k)
-		sum += ch;
-	return sum % table_size;
+	std::hash<Key> hashFunction;
+	return hashFunction(*k) % (this->size + 1);
 }
 
 template<class Key, class Data>
-inline int THashTable<Key, Data>::HashFunction(Key* k, int table_size) const
+inline int THashTable<Key, Data>::HashFunction(Key* k) const
 {
-	int sum = 0;
-	for (char ch : *k)
-		sum += ch;
-	return sum % table_size;
+	std::hash<Key> hashFunction;
+	return hashFunction(*k) % (this->size + 1);
 }
 
 template<class Key, class Data>
-inline int THashTable<Key, Data>::DoubleHashFunction(int table_size,int prev_index)
+inline int THashTable<Key, Data>::DoubleHashFunction(int prev_index)
 {
 	int p = 1;
-	return (prev_index + p) % table_size;
+	return (prev_index + p) % (this->size + 1);
 }
 
 template<class Key, class Data>
-inline int THashTable<Key, Data>::DoubleHashFunction(int table_size, int prev_index) const
+inline int THashTable<Key, Data>::DoubleHashFunction(int prev_index) const
 {
 	int p = 1;
-	return (prev_index + p) % table_size;
+	return (prev_index + p) % (this->size + 1);
 }
 
 template<class Key, class Data>
@@ -104,8 +115,8 @@ inline void THashTable<Key, Data>::Add(Key* k, Data* d)
 		throw "Table is full!!";
 	int num = -1;
 	int prev_index = 0;
-	int index = HashFunction(k, this ->size);
-	do
+	int index = HashFunction(k);
+	while(true)
 	{
 		if ((items[index].GetState() == 1) && (*(items[index].GetKey()) == *k))
 			throw "Duplication!!";
@@ -119,7 +130,7 @@ inline void THashTable<Key, Data>::Add(Key* k, Data* d)
 			this->count++;
 			break;
 		}
-		else if ((items[index].GetState() == 2) && (num > -1))
+		else if (((items[index].GetState() == 2) || (items[index].GetState() == 4)) && (num > -1))
 		{
 			items[num].SetState(1);
 			items[num].SetKey(k);
@@ -130,9 +141,9 @@ inline void THashTable<Key, Data>::Add(Key* k, Data* d)
 		else
 		{
 			prev_index = index;
-			index = DoubleHashFunction(this->size, prev_index);
+			index = DoubleHashFunction(prev_index);
 		}
-	} while (true);
+	}
 }
 
 template<class Key, class Data>
@@ -174,7 +185,7 @@ inline int THashTable<Key, Data>::FindInd(Key* k)
 {
 	int num = -1;
 	int prev_index = 0;
-	int index = HashFunction(k, this->size);
+	int index = HashFunction(k);
 	for (int i = 0; i < this->size; i++)
 	{
 		if ((items[index].GetState() == 1) && (*(items[index].GetKey()) == *k))
@@ -185,7 +196,7 @@ inline int THashTable<Key, Data>::FindInd(Key* k)
 		else
 		{
 			prev_index = index;
-			index = DoubleHashFunction(this->size, prev_index);
+			index = DoubleHashFunction(prev_index);
 		}
 	}
 	throw "Key is not found!!!";
@@ -196,7 +207,7 @@ inline int THashTable<Key, Data>::FindInd(Key* k) const
 {
 	int num = -1;
 	int prev_index = 0;
-	int index = HashFunction(k, this->size);
+	int index = HashFunction(k);
 	for (int i = 0; i < this->size; i++)
 	{
 		if ((items[index].GetState() == 1) && (*(items[index].GetKey()) == *k))
@@ -207,10 +218,22 @@ inline int THashTable<Key, Data>::FindInd(Key* k) const
 		else
 		{
 			prev_index = index;
-			index = DoubleHashFunction(this->size, prev_index);
+			index = DoubleHashFunction(prev_index);
 		}
 	}
 	throw "Key is not found!!!";
+}
+
+template<class Key, class Data>
+inline THashTableIterator<Key, Data> THashTable<Key, Data>::Begin()
+{
+	return THashTableIterator<Key, Data>(*this);
+}
+
+template<class Key, class Data>
+inline THashTableIterator<Key, Data> THashTable<Key, Data>::End()
+{
+	return THashTableIterator<Key, Data>(*this, this->GetCount());
 }
 
 #endif
